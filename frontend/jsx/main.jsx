9d87a19c5df1a6d5b0e8b118a87539b715185aea
Dmitry Gunin <kandidad@gmail.com>
2020-06-14 00:25:38 +0300

    Генерация шахматной доски

--- frontend/jsx/main.jsx
+++ frontend/jsx/main.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import {A, navigate} from 'hookrouter'
+import React from "react"
 import GetVariants from '../gql/board.graphql'
 import {useQuery} from '@apollo/react-hooks'
 import {convertCoordToNumber as convert} from './utils'
+import Square from './single_square.jsx'
 
 export default ({start = '0', variant = '0'}) => {
-    
+
     let s = convert(start)
 
     const { loading, error, data } = useQuery(GetVariants, {variables:{start: s}})
-    console.log(data)
     return(
-        <div>
-            <A href="/a8/1/"> a8 - 1 </A><br/>
-            <A href="/a8/2/"> a8 - 2 </A><br/>
-            <A href="/b8/1/"> b8 - 1 </A><br/>
-            <A href="/b8/2/"> b8 - 2 </A><br/>
+        <div className = "board">
             {loading&&<div>...Загрузка</div>}
             {error&&<div>Ошибка</div>}
-            {!loading&&data&&!!data.board.length&&<div>{data.board[+variant-1]}</div>}
+            {!error&&!loading&&data&&!!data.board.length&&
+                data.board[0].map((numb, i)=>{
+                    return <Square key={i} number = {i+1} value = {numb} />
+                })
+                }
         </div>
     )
 }
