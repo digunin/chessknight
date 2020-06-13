c394be5723baaede5d62332a163371cacaa34d3a
Dmitry Gunin <kandidad@gmail.com>
2020-06-14 00:22:04 +0300

    Компонент шахматной клетки

---
+++ frontend/jsx/single_square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {A} from 'hookrouter'
+import {convertNumberToCoord as convert} from './utils'
+
+export default ({subclass = "dark", number = 0, value = ""})=>{
+    return(
+        <div className = {"square " + subclass}>
+            <A href = {"/" + convert(number) + "/1/"}>{value}</A>
+        </div>
+    )
+}
