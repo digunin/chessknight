0425c17e99d7d8d333f44bce901c0dac07063842
Dmitry Gunin <kandidad@gmail.com>
2020-06-20 22:55:27 +0300

    Фоновое изображение

--- frontend/jsx/controlpanel.jsx
+++ frontend/jsx/controlpanel.jsx
@@ -7,9 +7,9 @@ export default ({onStart, onStop, onPause, onPlus, onMinus, speed})=>{
             <div onClick={onStart} className="controlbutton"><img src="/static/images/play.svg" alt=""/></div>
             <div onClick={onPause} className="controlbutton"><img src="/static/images/pause.svg" alt=""/></div>
             <div onClick={onStop} className="controlbutton"><img src="/static/images/stop.svg" alt=""/></div>
-            <div onClick={onPlus} className="controlbutton"><img src="/static/images/next.svg" alt=""/></div>
-            <Indicator speed = {speed} />
             <div onClick={onMinus} className="controlbutton"><img src="/static/images/previous.svg" alt=""/></div>
+            <Indicator speed = {speed} />
+            <div onClick={onPlus} className="controlbutton"><img src="/static/images/next.svg" alt=""/></div>
         </div>
     )
 }
