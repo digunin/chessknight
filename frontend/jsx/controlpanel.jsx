ac2dbca8b420aa9b21c6fc0196094dc6ddde02c9
Dmitry Gunin <kandidad@gmail.com>
2020-06-20 20:04:27 +0300

    Исправление некорректного отображения бордера в controlpanel

--- frontend/jsx/controlpanel.jsx
+++ frontend/jsx/controlpanel.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default ({onStart, onStop, onPause, onPlus, onMinus, period})=>{
     return(
         <div className="controlpanel">
-            <button onClick={onStart} className="controlbutton">Старт</button>
-            <button onClick={onPause} className="controlbutton">Пауза</button>
-            <button onClick={onStop} className="controlbutton">Стоп</button>
-            <button onClick={onPlus} className="controlbutton">+</button>
+            <div onClick={onStart} className="controlbutton">Старт</div>
+            <div onClick={onPause} className="controlbutton">Пауза</div>
+            <div onClick={onStop} className="controlbutton">Стоп</div>
+            <div onClick={onPlus} className="controlbutton">+</div>
             <div className="interval"><p>{period}</p><p>мс</p></div>
-            <button onClick={onMinus} className="controlbutton">-</button>
+            <div onClick={onMinus} className="controlbutton">-</div>
         </div>
     )
 }
