58bff889f8be6607f211f8a456cc6951219d2587
Dmitry Gunin <kandidad@gmail.com>
2020-06-24 22:29:13 +0300

    Декоратор ensure_csrf_cookie для метода get обеспечивает отправку в браузер csrf-токена

--- chessknight/views.py
+++ chessknight/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 class ChessknightView(View):
+    @method_decorator(ensure_csrf_cookie)
     def get(self, request, start = "0", variant = 0):
         if(start != "0"):
             if(int(variant)>10):
